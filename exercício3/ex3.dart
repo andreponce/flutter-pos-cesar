6819caaabfd82a2a337c7b030d1221f03d05d237
andreponce <andreluisponce@gmail.com>
2021-10-23 10:59:46 -0300

    ...

---
+++ exercício3/ex3.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+main(List<String> args){
+	const grades = [5, 8, 9, 5.5, 4.3, 2, 1, 9.8, 7.5, 8.5, 8.9, 9.3, 1.45];
+	var bestGrades = grades.where(isHightGrade);
+	print(bestGrades);
+}
+
+bool isHightGrade(grade){
+	return grade>8.5;
+}
